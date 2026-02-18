e91d9c12ebf40f19df72b646b7546bf646b5b3fc
mfwolffe <wolffemf@dukes.jmu.edu>
2026-02-17 21:52:48 -0500

    update gardesk rev: fix focus stuck after cross-monitor window move

--- flake.nix
+++ flake.nix
@@ -43,8 +43,8 @@
         # Shared source for gardesk suite (monorepo with submodules)
         gardesk-src = pkgs.fetchgit {
           url = "https://github.com/gardesk/gardesk";
-          rev = "f32dfa24c74de83cf3b250034d778f4cadd68955";
-          hash = "sha256-mrNzKY35q08lxuBbfsiryZZHK4KTLMQbiBqyvLorO5U=";
+          rev = "1f8968cec66a0a7e9df573de3a252ca90429cde7";
+          hash = "sha256-p8DCDxgaV48WD905umv6eRqMmM/zdxMbx+0zSiRNPio=";
           fetchSubmodules = true;
           name = "gardesk-src";
         };
